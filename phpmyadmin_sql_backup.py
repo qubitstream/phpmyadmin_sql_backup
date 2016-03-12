0d169f083e0ac9e5e848cd3c350a17d4a0dcedf9
Christoph Haunschmidt <c.haunschmidt@gmail.com>
2016-03-12 13:21:17 +0100

    added --prefix-format option and simple error reporting

--- phpmyadmin_sql_backup.py
+++ phpmyadmin_sql_backup.py
@@ -18,24 +18,29 @@
 # A Python script to automate the download of SQL dump backups
 # via a phpMyAdmin web interface.
 #
-# tested on Python 3.5
+# tested on Python 3.4+
+# requires: grab (http://grablib.org/)
 #
 # Christoph Haunschmidt 2016-03
 
 import argparse
 import os
 import re
+import sys
 import datetime
 
 import grab
 
-__version__ = '2016-03-10.0'
+__version__ = '2016-03-12.0'
 
 CONTENT_DISPOSITION_FILENAME_RE = re.compile(r'^.*filename="(?P<filename>[^"]+).*$')
+DEFAULT_PREFIX_FORMAT = r'%Y-%m-%d--%H-%M-%S-UTC_'
 
 
 def download_sql_backup(url, user, password, dry_run=False, overwrite_existing=False, prepend_date=True, basename='',
-                        output_directory=os.getcwd(), exclude_dbs=None, compression='none', timeout=60, **kwargs):
+                        output_directory=os.getcwd(), exclude_dbs=None, compression='none', prefix_format=None,
+                        timeout=60, **kwargs):
+    prefix_format = prefix_format or DEFAULT_PREFIX_FORMAT
     exclude_dbs = exclude_dbs.split(',') or []
     encoding = '' if compression == 'gzip' else 'gzip'
 
@@ -46,34 +51,40 @@ def download_sql_backup(url, user, password, dry_run=False, overwrite_existing=F
     g.doc.set_input_by_id('input_password', password)
     g.doc.submit()
 
-    g.doc.text_assert('server_export.php')
+    try:
+        g.doc.text_assert('server_export.php')
+    except Exception as e:
+        raise ValueError('Could not login - did you provide the correct username / password? ({})'.format(e))
     export_url = g.doc.select("id('topmenu')//a[contains(@href,'server_export.php')]/@href").text()
     g.go(export_url)
 
     dbs_available = [option.attrib['value'] for option in g.doc.form.inputs['db_select[]']]
+    dbs_to_dump = [db_name for db_name in dbs_available if db_name not in exclude_dbs]
+    if not dbs_to_dump:
+        print('Warning: no databases to dump (databases available: "{}")'.format('", "'.join(dbs_available)),
+            file=sys.stderr)
 
     file_response = g.doc.submit(
-        extra_post=[('db_select[]', db_name) for db_name in dbs_available if db_name not in exclude_dbs] + [
-            ('compression', compression)])
+        extra_post=[('db_select[]', db_name) for db_name in dbs_to_dump] + [('compression', compression)])
 
-    m = CONTENT_DISPOSITION_FILENAME_RE.match(g.response.headers['Content-Disposition'])
-    if not m:
+    re_match = CONTENT_DISPOSITION_FILENAME_RE.match(g.response.headers['Content-Disposition'])
+    if not re_match:
         raise ValueError(
             'Could not determine SQL backup filename from {}'.format(g.response.headers['Content-Disposition']))
 
-    content_filename = m.group('filename')
+    content_filename = re_match.group('filename')
     filename = content_filename if not basename else basename + os.path.splitext(content_filename)[1]
     if prepend_date:
-        prefix = datetime.datetime.utcnow().strftime(r'%Y-%m-%d--%H-%M-%S-UTC_')
+        prefix = datetime.datetime.utcnow().strftime(prefix_format)
         filename = prefix + filename
     out_filename = os.path.join(output_directory, filename)
 
     if os.path.isfile(out_filename) and not overwrite_existing:
         basename, ext = os.path.splitext(out_filename)
         n = 1
-        print('File {} already exists, to overwrite it use: --overwrite-existing'.format(out_filename))
+        print('File {} already exists, to overwrite it use --overwrite-existing'.format(out_filename), file=sys.stderr)
         while True:
-            alternate_out_filename = basename + '_({})'.format(n) + ext
+            alternate_out_filename = '{}_({}){}'.format(basename, n, ext)
             if not os.path.isfile(alternate_out_filename):
                 out_filename = alternate_out_filename
                 break
@@ -88,29 +99,42 @@ def download_sql_backup(url, user, password, dry_run=False, overwrite_existing=F
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Automates the download of SQL dump backups via a phpMyAdmin web interface.',
-        epilog='Written by Christoph Haunschmidt. Version: {}'.format(__version__))
+        epilog='Written by Christoph Haunschmidt, version: {}'.format(__version__))
 
-    parser.add_argument('url', help='phpMyAdmin login page url')
-    parser.add_argument('user', help='phpMyAdmin login user')
-    parser.add_argument('password', help='phpMyAdmin login password')
-    parser.add_argument('-o', '--output-directory', default=os.getcwd(), help='output directory for the SQL dump file')
+    parser.add_argument('url', metavar='URL', help='phpMyAdmin login page url')
+    parser.add_argument('user', metavar='USERNAME', help='phpMyAdmin login username')
+    parser.add_argument('password', metavar='PASSWORD', help='phpMyAdmin login password')
+    parser.add_argument('-o', '--output-directory', default=os.getcwd(),
+        help='output directory for the SQL dump file (default: the current working directory)')
     parser.add_argument('-p', '--prepend-date', action='store_true', default=False,
-        help='prepend the current UTC date + time to the filename: "YYYY-MM-DD--HH-MM-SS-UTC_"')
+        help='prepend current UTC date & time to the filename; see the --prefix-format option for custom formatting')
     parser.add_argument('-e', '--exclude-dbs', default='',
-        help='comma separated list for database names to exclude from the dump')
+        help='comma-separated list of database names to exclude from the dump')
     parser.add_argument('--compression', default='none', choices=['none', 'zip', 'gzip'],
-        help='compression method of the output file (default: %(default)s)')
+        help='compression method for the output file - must be supported by the server (default: %(default)s)')
     parser.add_argument('--basename',
-        help='the desired basename (without extension) of the SQL dump file (default: the name given by phpMyAdmin')
+        help='the desired basename (without extension) of the SQL dump file (default: the name given by phpMyAdmin)')
     parser.add_argument('--timeout', type=int, default=60,
         help='timeout in seconds for the requests (default: %(default)s)')
     parser.add_argument('--overwrite-existing', action='store_true', default=False,
-        help='overwrite existing backup files (instead of appending a number to the name)')
+        help='overwrite existing SQL dump files (instead of appending a number to the name)')
+    parser.add_argument('--prefix-format', default='',
+        help=str('the prefix format for --prepend-date (default: "{}"); in Python\'s strftime format. '
+                 'Must be used with --prepend-date to be in effect'.format(DEFAULT_PREFIX_FORMAT.replace('%', '%%'))))
     parser.add_argument('--dry-run', action='store_true', default=False,
-        help='dry run, do not actually download the dump')
+        help='dry run, do not actually download any file')
 
-    ARGS = parser.parse_args()
+    args = parser.parse_args()
 
-    dump_fn = download_sql_backup(**vars(ARGS))
+    if args.prefix_format and not args.prepend_date:
+        print('Error: --prefix-format given without --prepend-date', file=sys.stderr)
+        sys.exit(2)
 
-    print('{} saved SQL dump to: {}'.format(('Would have' if ARGS.dry_run else 'Successfully'), dump_fn))
+    try:
+        dump_fn = download_sql_backup(**vars(args))
+    except Exception as e:
+        print('Error: {}'.format(e), file=sys.stderr)
+        sys.exit(1)
+
+    print('{} saved SQL dump to: {}'.format(('Would have' if args.dry_run else 'Successfully'), dump_fn),
+        file=sys.stdout)
